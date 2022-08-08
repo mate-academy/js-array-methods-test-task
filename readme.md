5d404086ab7f1c1682fc0346e1817e79a2e43ef4
ArtemTopolskyi <topolskyiav@gmail.com>
2022-08-08 14:52:47 +0300

    basic task setup

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# Sum() - [put the task name here]
+# Array methods test
 [put a short description here] Implement the `sum` function returning a sum of two numbers
 
 **Read [the guideline](https://github.com/mate-academy/js_task-guideline/blob/master/README.md) before start**
---
+++ src/mothersWithChildren.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Implement getMothersWithChildren function
+ *
+ * Function returns array of mothers with 'children' array inside each object
+ *
+ * @param {object[]} people
+ *
+ * @return {object[]}
+ */
+function getMothersWithChildren(people) {
+  // write code here
+  return people;
+}
+
+module.exports = sum;
